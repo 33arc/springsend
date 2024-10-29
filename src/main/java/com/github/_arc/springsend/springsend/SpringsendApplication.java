7ed672ead6efe3dbb51da3f7ecec5bbeca5da6e4
33arc <quickselect@proton.me>
2024-10-29 18:54:50 +0300

    feat: initial commit

---
+++ src/main/java/com/github/_arc/springsend/springsend/SpringsendApplication.java
@@ -0,0 +1,13 @@
+package com.github._arc.springsend.springsend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/_arc/springsend/springsend/SpringsendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github._arc.springsend.springsend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
