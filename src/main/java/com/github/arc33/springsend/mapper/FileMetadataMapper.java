400b7375e59f03e8149f63d15b1034f47008971e
33arc <quickselect@proton.me>
2024-10-31 19:23:20 +0300

    feat: add FileMetadataMapper

---
+++ src/main/java/com/github/arc33/springsend/mapper/FileMetadataMapper.java
@@ -0,0 +1,68 @@
+package com.github.arc33.springsend.mapper;
+
+import com.github.arc33.springsend.dto.file.FileMetadataDto;
+import com.github.arc33.springsend.dto.file.FileUploadMetadata;
+import com.github.arc33.springsend.model.Category;
+import com.github.arc33.springsend.model.FileMetadata;
+import org.mapstruct.*;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE, imports = {LocalDateTime.class})
+public interface FileMetadataMapper {
+
+    @Mapping(target = "assignedUser", source = "assignedUser")
+    @Mapping(target = "publicAllow", expression = "java(!fileMetadata.expires())")
+    @Mapping(target = "categoryIds", expression = "java(mapCategoriesToIds(fileMetadata.categories()))")
+    FileMetadataDto toDto(FileMetadata fileMetadata);
+
+    @Mapping(target = "assignedUser", source = "assignedUser")
+    @Mapping(target = "expires", expression = "java(!fileMetadataDto.publicAllow())")
+    @Mapping(target = "fileKey", ignore = true)
+    @Mapping(target = "timestamp", expression = "java(java.time.LocalDateTime.now())")
+    @Mapping(target = "categories", ignore = true)
+    FileMetadata toEntity(FileMetadataDto fileMetadataDto);
+
+    @Mapping(target = "filename", source = "fileName")
+    @Mapping(target = "categories", ignore = true) // we will handle this in @AfterMapping
+    @Mapping(target = "timestamp", expression = "java(LocalDateTime.now())")
+    @Mapping(target = "fileKey", ignore = true)
+    FileMetadata toMetadata(FileUploadMetadata metadata);
+
+    @AfterMapping
+    default void mapUploadCategories(@MappingTarget FileMetadata fileMetadata, FileUploadMetadata fileMetadataDto) {
+        if (fileMetadataDto.getCategoryIds() != null) {
+            fileMetadata.setCategories(fileMetadataDto.getCategoryIds().stream()
+                    .map(id -> {
+                        Category category = new Category();
+                        category.setId(id);
+                        return category;
+                    })
+                    .collect(Collectors.toSet()));
+        }
+    }
+
+    @AfterMapping
+    default void mapCategories(@MappingTarget FileMetadata fileMetadata, FileMetadataDto fileMetadataDto) {
+        if (fileMetadataDto.getCategoryIds() != null) {
+            fileMetadata.setCategories(fileMetadataDto.getCategoryIds().stream()
+                    .map(id -> {
+                        Category category = new Category();
+                        category.setId(id);
+                        return category;
+                    })
+                    .collect(Collectors.toSet()));
+        }
+    }
+
+    default Set<Long> mapCategoriesToIds(Set<Category> categories) {
+        if (categories == null) {
+            return null;
+        }
+        return categories.stream()
+                .map(Category::getId)
+                .collect(Collectors.toSet());
+    }
+}
