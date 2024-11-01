f673c23bc15efbe34b31fb5cf15e6b1dfae3e764
33arc <quickselect@proton.me>
2024-11-01 20:48:04 +0300

    refactor: standardize FileManagementService interface

--- src/main/java/com/github/arc33/springsend/service/file/FileManagementService.java
+++ src/main/java/com/github/arc33/springsend/service/file/FileManagementService.java
@@ -1,5 +1,6 @@
 package com.github.arc33.springsend.service.file;
 
+import com.github.arc33.springsend.dto.file.FileDownloadResponse;
 import com.github.arc33.springsend.dto.file.FileMetadataDto;
 import com.github.arc33.springsend.dto.file.FileUploadMetadata;
 import com.github.arc33.springsend.model.Category;
@@ -12,19 +13,18 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.Optional;
 
 public interface FileManagementService {
-    Optional<FileMetadata> setFileMetadata(Long fileId, FileUploadMetadata metadata);
+    FileMetadataDto setFileMetadata(Long fileId, FileUploadMetadata metadata);
     FileMetadataDto getFileMetadataById(Long fileId);
 
-    FileMetadata uploadFile(MultipartFile file, FileUploadMetadata metadata);
-    PageImpl listAllFiles(Pageable pageable);
+    FileMetadataDto uploadFile(MultipartFile file, FileUploadMetadata metadata);
+    Page<FileMetadataDto> listAllFiles(Pageable pageable);
     boolean isFileOwnedByUser(Long fileId,String username);
-    byte[] getFileContent(Long fileId);
+    FileDownloadResponse getFileById(Long fileId);
     void updateFileContent(Long fileId, MultipartFile file);
     void deleteFile(Long fileId);
-    Page<FileMetadata> listUserFiles(String username, Pageable pageable);
-    Page<FileMetadataDto> searchFiles(String uploader, String assignedUser , String category, String searchTerm, int page, int size);
-    String getFilename(Long fileId);
+    Page<FileMetadataDto> listUserFiles(String username, Pageable pageable);
+    Page<FileMetadataDto> searchFiles(String uploader, String assignedUser, String category, String searchTerm, int page, int size);
 
-    void createCategory(Category category);
+    Category createCategory(Category category);
     Page<Category> listAllCategories(Pageable pageable);
 }
